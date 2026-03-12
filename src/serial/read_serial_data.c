d49893dadce1db10a56b1c6cb3c05aa7837047fd
Mark Fisher <mark.j.fisher@gmail.com>
2026-03-12 09:25:06 +0000

    Handle timeouts and complete frames in serial reading better

--- src/serial/read_serial_data.c
+++ src/serial/read_serial_data.c
@@ -11,18 +11,22 @@ extern uint8_t __fastcall__ read_rs423_char(void);
 // this is set to 0 for success, -1 for error in _read_rs423_char
 #define got_char    (ZP.cws_tmp[0])
 
-#define WAIT_MAX  ((uint16_t) 2000)
+#define SLIP_END          0xC0
+#define WAIT_FIRST_MAX    ((uint16_t) 50000)
+#define WAIT_NEXT_MAX     ((uint16_t) 2000)
 
 uint8_t read_serial_data(uint8_t *dst, uint16_t len, uint16_t *count) {
     uint8_t ch_byte;
     uint16_t wait_count;
+    uint16_t wait_limit;
     uint16_t i  = 0;
 
     while (i < len) {
         ch_byte    = 0;
         wait_count = 0;
+        wait_limit = (i == 0) ? WAIT_FIRST_MAX : WAIT_NEXT_MAX;
 
-        while (wait_count < WAIT_MAX) {
+        while (wait_count < wait_limit) {
             if (check_rs423_buffer() != 0) {
                 ch_byte = read_rs423_char();
                 break;
@@ -30,7 +34,7 @@ uint8_t read_serial_data(uint8_t *dst, uint16_t len, uint16_t *count) {
             ++wait_count;
         }
 
-        if (wait_count >= WAIT_MAX || got_char != 0) {
+        if (wait_count >= wait_limit || got_char != 0) {
             // do we want to pad?
             // pad zeros to the end
             // ch_byte = 0;
@@ -44,6 +48,14 @@ uint8_t read_serial_data(uint8_t *dst, uint16_t len, uint16_t *count) {
         dst[i] = ch_byte;
         ++i;
         ++(*count);
+
+        /* FujiBus packets are SLIP framed. Once we have seen the terminating
+         * END byte we already have the full frame, so avoid waiting for an
+         * idle timeout after every response.
+         */
+        if (i > 1 && ch_byte == SLIP_END) {
+            break;
+        }
     }
     return (*count == len) ? 1 : 0;
 }
