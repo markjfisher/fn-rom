34bb0015ffce344a8d5e9b2a43e046f04845cdb8
Mark Fisher <mark.j.fisher@gmail.com>
2026-04-23 08:53:24 +0100

    removed old code

--- src/serial/read_serial_data.h
+++
@@ -1,8 +0,0 @@
-#ifndef READ_SERIAL_DATA_H
-#define READ_SERIAL_DATA_H
-
-#include <stdint.h>
-
-// uint8_t read_serial_data(uint8_t *dst, uint16_t len, uint16_t *count);
-
-#endif // READ_SERIAL_DATA_H
